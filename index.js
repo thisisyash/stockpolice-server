8871a351268bdea73a7e49338e27239760296fd3
yash71720 <yash71720@gmail.com>
2025-05-06 12:57:13 +0530

    cors bug fix

--- index.js
+++ index.js
@@ -42,19 +42,7 @@ const allowedOrigins = [
   'https://stockpolice.app/'
 ];
 
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin (like mobile apps, curl, etc.)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true // if you use cookies or authentication
-}));
-
+app.use(cors());
 // This line ensures preflight requests are handled
 app.options('*', cors());
 
